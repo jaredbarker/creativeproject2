868e4725416837a72f5bd27d371c0b33920f9f64
Jared Barker <jaredbarker99@gmail.com>
2020-10-14 21:21:33 -0600

    Functionality working

---
+++ exchange-script.js
@@ -0,0 +1,28 @@
+fetch("https://open.exchangerate-api.com/v6/latest").then(function (response) {
+    return response.json();
+}).then(function (data) {
+    document.getElementById("header").innerHTML = data.rates.USD;
+    var countries = [];
+    for (let element in data.rates) {
+        countries.push(element);
+    }
+    console.log(data.rates);
+
+    document.getElementById("submit-button").addEventListener("click", function (event) {
+        event.preventDefault();
+        let start = document.getElementById("start").value;
+        let end = document.getElementById("end").value;
+        let startAmount = document.getElementById("start-amount").value;
+        if (start === "USD") {
+            let test = data.rates[end];
+            document.getElementById("result").innerHTML = startAmount * data.rates[end];
+        }
+        else if (end === "USD") {
+            document.getElementById("result").innerHTML = startAmount / data.rates[start];
+        }
+        else {
+            document.getElementById("result").innerHTML = (startAmount * data.rates[end]) / data.rates[start];
+        }
+    });
+
+});
